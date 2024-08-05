8e0c88da3d7f1bf41de5a6145cdecfc8b8fc4736
Aman Kumar Bairagi <amanbairagi1089@gmail.com>
2024-08-05 23:24:29 +0530

    few more file changes

--- utils/generateOGImage.js
+++ utils/generateOGImage.js
@@ -4,7 +4,6 @@ const path = require('path');
 
 const generateOGImage = async (title, content, imagePath) => {
     const browser = await puppeteer.launch({
-        executablePath: '/usr/bin/chromium-browser', 
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     const page = await browser.newPage();
